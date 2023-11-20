76679df606f0ccd597108bbe311a09921f1d179d
Amit261087 <amit.sharmaamit.sharma692@gmail.com>
2023-11-21 01:00:59 +0530

    test1

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '4rfmrf',
+  projectId: '5o6v5t',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
